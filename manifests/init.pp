c8f310200a416a2533e72a7bd2b09c70b0c198f0
Gerrard Geldenhuis <gerhardus.geldenhuis@gmail.com>
2016-07-12 14:33:58 +0100

    Adding index page to pretty things up

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,10 @@ class simple {
     docroot => '/var/www/simple',
   }
 
+  file { '/var/www/simple/index.html':
+    ensure  => file,
+    source  => 'modules:///simple/index.html',
+    require => Apache::Vhost['192.168.8.137'],
+  }
+
 }
